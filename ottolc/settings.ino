2b1405ff06fb996457fc171b84ffb4f8073ceefd
Andreas (@xAndy) Zimmermann <xandy@hackerspace-bamberg.de>
2017-03-27 18:43:43 +0200

    add initial setting storage support

---
+++ ottolc/settings.ino
@@ -0,0 +1,26 @@
+#include <EEPROM.h>
+
+enum class Setting{
+  trimLeftFoot,
+  trimRightFoot,
+  trimLeftLeg,
+  trimRightLeg,
+};
+
+// this is required as it will not build in more recent arduino versions
+// as they add all function prototypes at the beginning and the 
+// enum will be unknown at that point. defining the prototype here
+// fixes this and the build will run
+char readSetting(Setting key);
+
+char readSetting(Setting key){
+  return EEPROM.read(int(key));
+}
+void writeSetting(Setting key, char value);
+void writeSetting(Setting key, char value){
+  Serial.print("writing to settings ");
+  Serial.print(int(key), DEC);
+  Serial.print(" ");
+  Serial.println(value, DEC);
+  EEPROM.write(int(key), value);
+}
