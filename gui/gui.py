dd963026f78f7ad38e7e80c7edec5017f19b06bf
Andreas (@xAndy) Zimmermann <xandy@hackerspace-bamberg.de>
2017-05-19 20:55:14 +0200

    autopep

--- gui/gui.py
+++ gui/gui.py
@@ -4,7 +4,7 @@ from gui.app import App
 if __name__ == "__main__":
     root = tkinter.Tk()
     root.configure()
-    if len(sys.argv)>1:
+    if len(sys.argv) > 1:
         app = App(root, sys.argv[1])
     else:
         app = App(root)
